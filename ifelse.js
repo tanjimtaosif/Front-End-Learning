c3ceff3d60f9ae9aed44a4b2c9351d4bc842a478
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-29 02:45:28 +0600

    Learned If else condition

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+var name = 'Tanjim';
+var age = 65;
+
+if (age < 13) {
+    console.log(name + " is a child.");
+}
+else if (age >= 13 && age <= 25) {
+    console.log(name + " is young.");
+}
+else {
+    console.log(name + " is old.");
+    console.log("Wisdom comes with age!");
+    console.log("Hope you're enjoying your golden years!");
+}
