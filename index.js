b6970513ab5aa72fc8445fd6120f5915f1ae1395
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-29 02:13:42 +0600

    Data Types

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var age = 30;
+//to check data type we use typeof
+var age = 30; //Number data type
+console.log(typeof age); 
 
-console.log(age);
+var Age = 'Thirteen'; //String data type
+console.log(typeof age);
+
+var status = true;
+console.log(typeof status); //Boolean dat type
