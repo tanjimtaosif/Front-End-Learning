ab69e27d955af803afd600543900b29574816c18
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-29 16:05:08 +0600

    Arrow Function

---
+++ arrow-function.js
@@ -0,0 +1,25 @@
+const hello = () => {
+    console.log("Hello There");
+}
+
+hello();
+
+// 1. Concise syntax
+
+let sum = (a, b) => a + b;
+console.log(sum(12, 34));
+
+//2. deal with objects
+let objreturn = () => ({ name: "smith" }); //it doesn't directly return objects 
+console.log(objreturn().name);
+
+// 3. Deal with this keyword
+let thiskey = () => this;
+console.log(thiskey());
+
+//4. Getting arguments
+let arg = (...arguments) => {
+    console.log(arguments)
+}
+
+arg(4, 3, 5, 32);
