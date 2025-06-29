efdc9524745ff7dd83cd2929fd292a00e18c1f3c
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-29 16:05:40 +0600

    Declaration Function

---
+++ declaration-fucntion.js
@@ -0,0 +1,14 @@
+// Satement function and Named Function
+function person() {
+    // console.log("Example function");
+    return "This is return Message";
+}
+
+let message = person();
+console.log(message);
+
+function sum(a, b){
+    return a+b;
+}
+let result = sum (4, 9);
+console.log(result);
