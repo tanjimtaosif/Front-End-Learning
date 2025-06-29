9f0f8ecda3ead4097eb08125294cc5e9eaec357a
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-29 16:05:26 +0600

    Expression Function

---
+++ expression-function.js
@@ -0,0 +1,11 @@
+//Function Anonymous 
+// let add = function (num1, num2) {
+//     return num1 + num2
+// }
+
+// let result = add(55, 65)
+// console.log(result)
+
+(function(a, b){
+    console.log(a+b);
+})(2, 4);
