a2382826ca96d68377037bd508fc469d9edf5e0a
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-30 11:11:12 +0600

    Variables

---
+++ JS Basics/variables.js
@@ -0,0 +1,14 @@
+const accoundId = 144553;
+let accountEmail = "Email@gmail.com";
+var accountPassword = "12345";
+accountCity = "Munshiganj";
+
+// accountId = 2 // not allowed
+accountEmail="hi@gmail.com";
+accountPassword="54321";
+var accountCity="Narayanganj";
+let accountphone;
+
+console.log(accoundId);
+console.table([accountPassword, accountCity, accountEmail,accountphone]);
+
