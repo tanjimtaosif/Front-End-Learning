9e76f9124cba8e37dfbf991c16433c5a693e0012
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-30 11:59:55 +0600

    Convertion

---
+++ JS Basics/convertonOperation.js
@@ -0,0 +1,18 @@
+// let age = 33;
+// let score = "23";
+
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+
+let isBoolean = 0;
+
+let booleanIsLoggedIn = Boolean(isBoolean);
+console.log(booleanIsLoggedIn);
+
+// "1" => true
+// "0" => false
