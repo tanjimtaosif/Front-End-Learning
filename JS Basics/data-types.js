bbe770c248e9077dd42be342d515b1e070da20c5
tanjimtaosif <itsmahtab007@gmail.com>
2025-06-30 11:59:37 +0600

    Data Types

---
+++ JS Basics/data-types.js
@@ -0,0 +1,14 @@
+let name = "Mahtab";
+let age = 18 ;
+let isLoggedIn = false;
+
+//  number => 2 to 53
+// bigint
+// string => ""
+// null => Standalone value, its itself a object
+// undefined =>
+// symbol =>unique
+
+console.log(typeof age);
+console.log(typeof null);
+
