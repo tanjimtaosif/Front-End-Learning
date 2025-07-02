fdca887d19bbb6dc7ac44086d6a73e27bd742fd4
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-02 12:33:36 +0600

    Basic JS

--- JS Fundamentals/ifelse.js
+++ JS Fundamentals/ifelse.js
@@ -12,3 +12,37 @@ else {
     console.log("Wisdom comes with age!");
     console.log("Hope you're enjoying your golden years!");
 }
+
+// x = 2;
+// y = 4;
+
+// console.log(x === 2);
+
+// // == -> Only check values
+// // === -> Checks value and type also
+
+
+//Number greater and equal 80=> print A+
+//Number greater and equal 70=> print A 
+//Number greater and equal 60=> print A- 
+//Number greater and equal 50=> print B+
+//Other Fail 
+
+const number = 89;
+
+if (number >= 80) {
+    console.log("A+");
+}
+else if (number >= 70) {
+    console.log("A");
+}
+else if (number >= 60) {
+    console.log("A-");
+}
+else if (number >= 50) {
+    console.log("B+");
+}
+else {
+    console.log("Fail");
+
+}
