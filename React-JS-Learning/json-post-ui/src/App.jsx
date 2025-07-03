0f92095837e9d0cfcf9853ee5bb5e615b26658e4
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-03 11:36:39 +0600

    Updated

--- React-JS-Learning/json-post-ui/src/App.jsx
+++ React-JS-Learning/json-post-ui/src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [info, setInfo] = useState({
+    name: '',
+    address: '',
+    phone: ''
+  });
+
+  //pop-up when click 5 times
+  useEffect(() => {
+    if (count > 0 && count % 5 === 0) {
+      alert(`You have clicked ${count} times!`);
+    }
+  }, [count]);
+
+  //load personal info
+  useEffect(() => {
+    setTimeout(() => {
+      setInfo({
+        name: 'Tanjim Mahtab',
+        address: 'Dhaka, Bangladesh',
+        phone: '+8801646765439'
+      });
+    }, 1000);
+  }, []);
+
+  const style = {
+    color: 'white',
+    backgroundColor: '#456882'
+  }
 
   return (
     <>
@@ -16,7 +45,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <div className='my-info'>
+        <h2 style={style}>My Information</h2>
+        <p><strong>Name:</strong> {info.name || "Loading..."}</p>
+        <p><strong>Address:</strong> {info.address || "Loading..."}</p>
+        <p><strong>Phone:</strong> {info.phone || "Loading..."}</p>
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,6 +61,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
