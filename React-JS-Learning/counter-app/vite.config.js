28334606527dcd06ed325b7eb598ca3c7fdb53a1
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-03 11:46:57 +0600

    Added tailwind

--- React-JS-Learning/counter-app/vite.config.js
+++ React-JS-Learning/counter-app/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
 
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+})
