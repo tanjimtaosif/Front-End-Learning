f1c4cfc93d1a3d5f646bd8054651a056b60144f4
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-28 21:55:31 +0600

    Reassignment and Redeclaration

---
+++ About JavaScript/reassignment-redeclaration.js
@@ -0,0 +1,9 @@
+// Reassignment and Redeclaration
+
+var a = 12;
+a = 32;       //  Reassign
+var a = 46;   //  Redeclare
+
+let x = 30;
+x = 40;       //  Reassign
+// let x = 330; // Redeclaration not allowed
