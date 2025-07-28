2a6cdadfccc2280ec63f2148c41d32156c943a29
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-28 21:53:46 +0600

    basic examples of variable declaration and initialization using var

---
+++ About JavaScript/declaration-initialization.js
@@ -0,0 +1,6 @@
+// Variable Declaration & Initialization (ES5 vs ES6)
+var username = "tanjim"; // ES5 var declaration
+
+// Implicit global (not recommended)
+a = 12; // Avoid using variables without declaration
+var a;  // Declaring later (gets hoisted)
