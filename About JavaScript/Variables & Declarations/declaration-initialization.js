8740d61ed2e9e69ed8686748ed31202bccda7a6b
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-28 23:47:08 +0600

    Updated

--- About JavaScript/Variables & Declarations/declaration-initialization.js
+++
@@ -1,6 +0,0 @@
-// Variable Declaration & Initialization (ES5 vs ES6)
-var username = "tanjim"; // ES5 var declaration
-
-// Implicit global (not recommended)
-a = 12; // Avoid using variables without declaration
-var a;  // Declaring later (gets hoisted)
--- About JavaScript/Variables & Declarations/reassignment-redeclaration.js
+++
@@ -1,9 +0,0 @@
-// Reassignment and Redeclaration
-
-var a = 12;
-a = 32;       //  Reassign
-var a = 46;   //  Redeclare
-
-let x = 30;
-x = 40;       //  Reassign
-// let x = 330; // Redeclaration not allowed
